6c248b0596a10e2cad139208c98c7f0499848dd3
Kmunoz2022 <kmunoz2022@gmail.com>
2024-08-17 16:35:34 -0400

    AllEmployees

---
+++ src/components/containers/AllEmployeesContainer.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AllEmployeesView from "../views/AllEmployeesView";
+
+const AllEmployeesContainer = () => {
+  return <AllEmployeesView />;
+};
+
+export default AllEmployeesContainer;
---
+++ src/components/views/AllEmployeesView.jsx
@@ -0,0 +1,9 @@
+function AllEmployeesView (props) {
+    console.log(props);
+    return (
+        <h4>Hello</h4>
+    );
+
+}
+
+export default AllEmployeesView;
